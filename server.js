87efcb0585268bc8218f55924a9932321a59efc9
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-21 04:47:46 +1000

    Define response to display upload file details

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/'});
 
 var app = express();
 
@@ -16,6 +16,15 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse/', upload.single('upfile'), function(req, res) {
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
